aabb326ec2b547cb1d7e4b0bed3cfe4ef4395c77
GwangBong Lee <leegb93@gmail.com>
2022-11-22 22:36:43 +0900

    Update App.js

--- off-the-chain-bulldogz-season1/src/App.js
+++ off-the-chain-bulldogz-season1/src/App.js
@@ -201,7 +201,7 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bulldogsCar.gif" : null}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bulldogsCar.png" : null}
       >
         <a href={CONFIG.MARKETPLACE_LINK}>
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
